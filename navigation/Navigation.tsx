7afdf97e8e598f0303d5bba0fa1928c8d33e956e
EZhitnikovich <ezhitnikovich@gmail.com>
2023-08-15 00:10:02 +0300

    add navigation

---
+++ navigation/Navigation.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import HomeScreen from "../screens/HomeScreen";
+import TestScreen from "../screens/TestScreen";
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Text" component={TestScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
