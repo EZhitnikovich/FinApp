f2b70c3de66b5dc9b9bb1e0c33958bf89fcf0371
EZhitnikovich <ezhitnikovich@gmail.com>
2023-08-19 13:36:10 +0300

    complete homescreen layout

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,9 @@
 import {
   View,
   Text,
-  ScrollView,
   TouchableOpacity,
   DeviceEventEmitter,
+  Image,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -15,13 +15,15 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { storageKey } from "../utils/constants";
 import { getData, storeData } from "../utils/asyncStorage";
 import {
-  CreditCardIcon,
-  MagnifyingGlassPlusIcon,
+  AdjustmentsVerticalIcon,
+  BookmarkIcon,
+  CalendarIcon,
+  ClipboardDocumentListIcon,
+  MinusIcon,
+  PlusIcon,
 } from "react-native-heroicons/outline";
-import { Card } from "../components/card";
-import { AddCardButton } from "../components/AddCardButton";
-import { Chart } from "../components/chart";
-import { Legend } from "../components/legend";
+import { StatusBar } from "expo-status-bar";
+import { theme } from "../theme";
 
 export default function HomeScreen() {
   const navigation =
@@ -51,83 +53,137 @@ export default function HomeScreen() {
     });
   }, []);
 
-  return !loading ? (
-    accounts && accounts.length > 0 ? (
-      <SafeAreaView className="flex h-full">
-        {/* accounts */}
-        {/* TODO: make partial accounts load */}
-        <ScrollView
-          className="flex-grow-0"
-          persistentScrollbar={true}
-          showsHorizontalScrollIndicator={true}
-          horizontal={true}
-          pagingEnabled={true}
-        >
-          {accounts.map((item, index) => (
-            <Card
-              account={item}
-              key={index}
-              onAccountSelected={(acc) => selectAccount(acc)}
-            />
-          ))}
-          {/* add account */}
-          <View className="w-screen">
-            <AddCardButton
-              onPress={() =>
-                navigation.navigate("AddCard", { accounts: accounts })
-              }
-            />
+  return (
+    <View className="relative flex-1">
+      <StatusBar style="light" />
+      <Image
+        source={require("../assets/images/bg.png")}
+        className="absolute h-full w-full"
+      />
+      <SafeAreaView className="flex flex-1">
+        <View className="h-[10%] flex-row justify-between items-end pb-3 px-4">
+          <TouchableOpacity className="h-14 w-14 rounded-lg border border-purple-950 bg-purple-700">
+            <CalendarIcon className="m-auto" size={53} color={"white"} />
+          </TouchableOpacity>
+          <View className="items-center">
+            <Text className="mx-auto text-purple-300">August 18, 2023</Text>
+            <Text className="text-purple-100">Account name</Text>
           </View>
-        </ScrollView>
-        {/* info */}
-        {currentAccount ? (
-          <View className="flex-1 border rounded-2xl p-3 m-5">
-            <Text className="text-center text-3xl font-semibold border-b">
-              {currentAccount?.name}
+          <TouchableOpacity className="h-14 w-14 rounded-lg border border-purple-950 bg-purple-700">
+            <AdjustmentsVerticalIcon
+              className="m-auto"
+              size={53}
+              color={"white"}
+            />
+          </TouchableOpacity>
+        </View>
+        <View
+          className="flex-1"
+          style={{ backgroundColor: theme.bgWhite(0.9) }}
+        >
+          {/* balance */}
+          <View className="h-[15%] w-[80%] bg-purple-200 m-auto rounded-xl items-center my-3">
+            <Text className="text-green-700 m-auto text-lg">
+              +9999999999999999
             </Text>
-            <View className="flex-row mt-5 justify-between">
-              <Text className="text-2xl">Balance:</Text>
-              <View className="flex-row justify-end">
-                <Text className="text-2xl pr-2">
-                  {currentAccount.balance.toString().length > 15
-                    ? currentAccount.balance.toString().slice(0, 14) + "..."
-                    : currentAccount.balance.toString()}
-                </Text>
-                <CreditCardIcon size={32} color={"black"} />
+            <Text className="text-3xl m-auto">9999999999999999</Text>
+            <Text className="text-red-700 m-auto text-lg">
+              -9999999999999999
+            </Text>
+          </View>
+          {/* chart */}
+          {/* TODO: remove temp items */}
+          <View className="flex-1 pb-6">
+            <View className="rounded-full w-[250] h-[250] bg-purple-400 mx-auto mt-auto">
+              <View className="rounded-full w-[150] h-[150] bg-white m-auto"></View>
+            </View>
+          </View>
+          {/* legend */}
+          {/* TODO: remove temp items */}
+          <View className="bg-purple-300 h-[20%] w-[80%] mx-auto rounded-2xl px-3 py-1">
+            <View className="flex-row">
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
+              </View>
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
+              </View>
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
               </View>
             </View>
-            {/* chart */}
-            <Chart
-              categories={currentAccount.categories}
-              history={currentAccount.history}
-            />
-            {currentAccount.categories.length > 0 ? (
-              <View className="flex-grow-0 h-[30%] mb-2 border rounded-2xl p-3">
-                <Legend categories={currentAccount.categories} />
+            <View className="flex-row">
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
               </View>
-            ) : null}
-            {/* control */}
-            <TouchableOpacity className="flex-row border rounded-2xl justify-center p-2 mt-2">
-              <MagnifyingGlassPlusIcon size={28} color="black" />
-              <Text className="text-xl">More</Text>
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
+              </View>
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
+              </View>
+            </View>
+            <View className="flex-row">
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
+              </View>
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
+              </View>
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
+              </View>
+            </View>
+            <View className="flex-row">
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
+              </View>
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
+              </View>
+              <View className="flex-row pb-2">
+                <BookmarkIcon size={25} color={"black"} />
+                <Text className="my-auto pl-2 pr-2">Category</Text>
+              </View>
+            </View>
+          </View>
+          {/* control */}
+          <View className="flex-row justify-between w-[80%] py-6 flex-grow-0 mx-auto">
+            {/* expenses */}
+            <TouchableOpacity
+              className="w-24 h-24 border-4 border-red-600 rounded-2xl items-center justify-center"
+              style={{ backgroundColor: theme.bgWhite(0.5) }}
+            >
+              <MinusIcon size={60} color={"red"} />
+            </TouchableOpacity>
+            {/* history */}
+            <TouchableOpacity
+              className="w-24 h-24 border-4 rounded-2xl items-center justify-center"
+              style={{ backgroundColor: theme.bgWhite(0.5) }}
+            >
+              <ClipboardDocumentListIcon size={60} color={"black"} />
+            </TouchableOpacity>
+            {/* income */}
+            <TouchableOpacity
+              className="w-24 h-24 border-4 border-green-600 rounded-2xl items-center justify-center"
+              style={{ backgroundColor: theme.bgWhite(0.5) }}
+            >
+              <PlusIcon size={60} color={"green"} />
             </TouchableOpacity>
           </View>
-        ) : (
-          <View className="flex flex-1 items-center justify-center border rounded-2xl m-5">
-            <Text className="font-semibold text-4xl">Select account</Text>
-          </View>
-        )}
+        </View>
       </SafeAreaView>
-    ) : (
-      <SafeAreaView className="flex h-full">
-        <AddCardButton
-          onPress={() => navigation.navigate("AddCard", { accounts: [] })}
-        />
-      </SafeAreaView>
-    )
-  ) : (
-    <SafeAreaView className="flex flex-1 items-center justify-center">
-      <Text className="font-semibold text-4xl">Loading...</Text>
-    </SafeAreaView>
+    </View>
   );
 }
---
+++ theme/index.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  bgWhite: (opacity: number) => {
+    return `rgba(255,255,255, ${opacity})`;
+  },
+
+  bgPurple: (opacity: number) => {
+    return `rgba(180,30,180, ${opacity})`;
+  },
+};
